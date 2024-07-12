2393f4d2715a62367b1ca51d32f2ac9c84764a67
anlhu <anlhu@ucsc.edu>
2024-07-12 03:35:45 +0000

    whoops i deleted too much in the merge conflict resolve

--- backend/src/main/java/com/example/casa/Model/Organization.java
+++ backend/src/main/java/com/example/casa/Model/Organization.java
@@ -7,6 +7,14 @@ import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "organization")
 public class Organization {
--- backend/src/main/java/com/example/casa/Model/User.java
+++ backend/src/main/java/com/example/casa/Model/User.java
@@ -7,6 +7,19 @@ import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "users")
 public class User {
