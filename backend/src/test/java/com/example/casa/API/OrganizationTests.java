774a72b3e8f71e84288732e544260baebd7a6c40
anlhu <anlhu@ucsc.edu>
2024-07-12 03:00:34 +0000

    refactor and outline remaining unit tests

--- backend/src/test/java/com/example/casa/API/OrganizationTests.java
+++ backend/src/test/java/com/example/casa/API/OrganizationTests.java
@@ -1,6 +1,7 @@
 package com.example.casa.API;
 
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -25,6 +26,8 @@ import com.example.casa.Payload.SignUpRequest;
 import com.example.casa.Repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.servlet.ServletException;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // Allows non-static @BeforeAll
@@ -42,94 +45,113 @@ public class OrganizationTests {
     @Autowired
     private UserRepository userRepository;
 
-    private String newUserEmail;
-    private String newUserPassword;
-    private String token;
-    private String userId;
+    private String waltToken;
+    private String waltId;
+    private String jessieToken;
+    private String jessieId;
 
-    @BeforeAll
-    void setup() throws Exception {
-        newUserEmail = "walter@white.com";
-        newUserPassword = "password";
-
-        // Signup request
+    private void signUpRequest(String first, String last, String email, String pass) throws Exception {
         SignUpRequest signUpRequest = new SignUpRequest();
-        signUpRequest.setFirstName("Walter");
-        signUpRequest.setLastName("White");
-        signUpRequest.setEmail(newUserEmail);
-        signUpRequest.setPassword(newUserPassword);
+        signUpRequest.setFirstName(first);
+        signUpRequest.setLastName(last);
+        signUpRequest.setEmail(email);
+        signUpRequest.setPassword(pass);
 
-        String signupJson = objectMapper.writeValueAsString(signUpRequest);
+        String jsonString = objectMapper.writeValueAsString(signUpRequest);
 
-        // Perform signup
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(signupJson))
+                .content(jsonString))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
 
-        // Login request to get token
+    private String loginRequest(String email, String pass) throws Exception {
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail(newUserEmail);
-        loginRequest.setPassword(newUserPassword);
+        loginRequest.setEmail(email);
+        loginRequest.setPassword(pass);
 
         String loginJson = objectMapper.writeValueAsString(loginRequest);
 
-        // Perform login
         ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(loginJson))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
-        // Verify login response and save token
         AuthResponse authResponse = objectMapper.readValue(result.andReturn().getResponse().getContentAsString(), AuthResponse.class);
-        token = authResponse.getAccessToken();
+        return authResponse.getAccessToken();
+    }
 
-        // Use token to get userId
+    private String getUserId(String token, String email) throws Exception {
         ResultActions userDataResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/me")
                 .header("Authorization", "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(newUserEmail))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(email))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
 
-        // Deserialize and extract userId
-        //      It won't deserialize with objectMapper
-        //      Cannot handle managed/back reference 'userOrgs': back reference type (`java.util.Set<com.example.casa.Model.User>`) not compatible with managed type (com.example.casa.Model.User) at [Source: (String)"{"id":"70e3198f-ff71-41dc-a633-d01b79c36526","firstName":"Walter","lastName":"White","email":"walter@white.com","password":"{bcrypt}$2a$10$23nCy68.GLZEv9r6S5dPd.ggYE18EJIm31.6BwsHok5jvYrzDvDt.","imageUrl":null,"provider":"local","providerId":null,"organizations":[]}"; line: 1, column: 1]
+        // Deserialize and extract waltId
+        //      It won't deserialize with objectMapper, gives error:
+        //          Cannot handle managed/back reference 'userOrgs': back reference type (`java.util.Set<com.example.casa.Model.User>`) not compatible with managed type (com.example.casa.Model.User) at [Source: (String)"{"id":"70e3198f-ff71-41dc-a633-d01b79c36526","firstName":"Walter","lastName":"White","email":"walter@white.com","password":"{bcrypt}$2a$10$23nCy68.GLZEv9r6S5dPd.ggYE18EJIm31.6BwsHok5jvYrzDvDt.","imageUrl":null,"provider":"local","providerId":null,"organizations":[]}"; line: 1, column: 1]
         //      So temp workaround and just extract it by looking at the serialized json string
         //      Rn it has format "{"id":"user-id-here-in-this-form","firstName":"Walter",...}"
         // User userDataResponse = objectMapper.readValue(userDataResult.andReturn().getResponse().getContentAsString(), User.class);
-        // userId = userDataResponse.getId();
+        // waltId = userDataResponse.getId();
         String jsonString = userDataResult.andReturn().getResponse().getContentAsString();
-        String idStart = "\"id\":\"";
-        int indexOfId = jsonString.indexOf(idStart);
-        int indexOfEndOfId = jsonString.indexOf('\"', indexOfId + idStart.length());
-        userId = jsonString.substring(indexOfId + idStart.length(), indexOfEndOfId);
-        // System.out.println(userId);
+        return extractJsonString(jsonString, "id");
+    }
+
+    private String extractJsonString(String jsonString, String key) {
+        String keyInJson = '"' + key + '"' + ':' + '"';
+        int indexOfId = jsonString.indexOf(keyInJson);
+        int indexOfEndOfId = jsonString.indexOf('"', indexOfId + keyInJson.length());
+        return jsonString.substring(indexOfId + keyInJson.length(), indexOfEndOfId);
+    }
+
+    @BeforeAll
+    void setup() throws Exception {
+        // Send signup requests
+        signUpRequest("Walter", "White", "walter@white.com", "password");
+        signUpRequest("Jessie", "Pinkman", "jessie@pinkman.com", "password");
+
+        // Send login requests
+        waltToken = loginRequest("walter@white.com", "password");
+        jessieToken = loginRequest("jessie@pinkman.com", "password");
+
+        // Use token to get userId
+        waltId = getUserId(waltToken, "walter@white.com");
+        jessieId = getUserId(jessieToken, "jessie@pinkman.com");
     }
 
     @Test
-    void userGetAndCreateOrganization() throws Exception {
+    void organizationGetCreatePutDeleteInvite() throws Exception {
+        // Organization to make
         OrganizationDto organizationDto = new OrganizationDto();
         organizationDto.setOrgName("Chemistry Club");
         organizationDto.setOrgDescription("Not cooking meth");
         organizationDto.setOrgLocation("Albuquerque");
-
         String organizationJson = objectMapper.writeValueAsString(organizationDto);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/" + userId + "/organizations")
-                .header("Authorization", "Bearer " + token)
+        // Get nonexistant org
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/" + waltId + "/organizations")
+                .header("Authorization", "Bearer " + waltToken)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/" + userId + "/organizations")
-                .header("Authorization", "Bearer " + token)
+        // create new org
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/" + waltId + "/organizations")
+                .header("Authorization", "Bearer " + waltToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(organizationJson))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.orgName").value("Chemistry Club"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgName").value("Chemistry Club"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgDescription").value("Not cooking meth"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgLocation").value("Albuquerque"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgId").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.users").doesNotExist());
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/" + userId + "/organizations")
-                .header("Authorization", "Bearer " + token)
+        // get new org
+        ResultActions newOrg = mockMvc.perform(MockMvcRequestBuilders.get("/user/" + waltId + "/organizations")
+                .header("Authorization", "Bearer " + waltToken)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].orgName").value("Chemistry Club"))
@@ -137,31 +159,76 @@ public class OrganizationTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].orgLocation").value("Albuquerque"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].orgId").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].users").doesNotExist());
+        String orgId = extractJsonString(newOrg.andReturn().getResponse().getContentAsString(), "orgId");
+
+        // update org
+        OrganizationDto organizationUpdateDto = new OrganizationDto();
+        organizationUpdateDto.setOrgName("Meth Club");
+        organizationUpdateDto.setOrgDescription("Make meth");
+        organizationUpdateDto.setOrgLocation("Albuquerque");
+        String organizationUpdateJson = objectMapper.writeValueAsString(organizationUpdateDto);
+        mockMvc.perform(MockMvcRequestBuilders.put("/organization/" + orgId)
+                .header("Authorization", "Bearer " + waltToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(organizationUpdateJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgName").value("Meth Club"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgDescription").value("Make meth"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgLocation").value("Albuquerque"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orgId").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.users").doesNotExist());
+
+        // Get users - only Walter is in
+        mockMvc.perform(MockMvcRequestBuilders.get("/organization/" + orgId + "/users")
+                .header("Authorization", "Bearer " + waltToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(waltId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1]").doesNotExist());
+
+        // Invite user Jessie Pinkman
+        // Get users - Walter and Jessie
+        mockMvc.perform(MockMvcRequestBuilders.get("/organization/" + orgId + "/users")
+                .header("Authorization", "Bearer " + waltToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(waltId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(jessieId));
+
+        // Get org works for Jessie
+        // Remove user Jessie Pinkman
+        // Get users - only Walter is in
+        // Delete org
+        mockMvc.perform(MockMvcRequestBuilders.delete("/organization/" + orgId)
+                .header("Authorization", "Bearer " + waltToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // Get org doesnt show
+        // mockMvc.perform(MockMvcRequestBuilders.get("/user/" + waltId + "/organizations")
+        //         .header("Authorization", "Bearer " + token)
+        //         .contentType(MediaType.APPLICATION_JSON))
+        //         .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
-    // @Test
-    // void testUpdateOrganization() throws Exception {
-    //     OrganizationDto organizationDto = new OrganizationDto();
-    //     organizationDto.setOrgName("New Chemistry Club");
-    //     organizationDto.setOrgDescription("Updated description");
-    //     organizationDto.setOrgLocation("New Location");
-    //     String organizationJson = objectMapper.writeValueAsString(organizationDto);
-    //     String orgId = "organization-id"; // Replace with actual organization ID from your test setup
-    //     mockMvc.perform(MockMvcRequestBuilders.put("/organization/" + orgId)
-    //             .header("Authorization", "Bearer " + token)
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(organizationJson))
-    //             .andExpect(MockMvcResultMatchers.status().isOk())
-    //             .andExpect(MockMvcResultMatchers.jsonPath("$.orgName").value("New Chemistry Club"));
-    // }
-    // @Test
-    // void testDeleteOrganization() throws Exception {
-    //     String orgId = "organization-id"; // Replace with actual organization ID from your test setup
-    //     mockMvc.perform(MockMvcRequestBuilders.delete("/organization/" + orgId)
-    //             .header("Authorization", "Bearer " + token)
-    //             .contentType(MediaType.APPLICATION_JSON))
-    //             .andExpect(MockMvcResultMatchers.status().isOk());
-    // }
+    @Test
+    void updateNonexistantOrganization() throws Exception {
+        OrganizationDto organizationUpdateDto = new OrganizationDto();
+        organizationUpdateDto.setOrgName("Meth Club");
+        organizationUpdateDto.setOrgDescription("Make meth");
+        organizationUpdateDto.setOrgLocation("Albuquerque");
+        String organizationUpdateJson = objectMapper.writeValueAsString(organizationUpdateDto);
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.put("/organization/" + "0")
+                    .header("Authorization", "Bearer " + waltToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(organizationUpdateJson))
+                    .andExpect(MockMvcResultMatchers.status().is(69420)); // must crash before it reaches here
+        } catch (ServletException e) {
+            assertTrue(e.getMessage().contains("Request processing failed: java.lang.RuntimeException: Organization not found with id: 0"));
+        }
+    }
+
     // @Test
     // void testInviteUserToOrganization() throws Exception {
     //     String orgId = "organization-id"; // Replace with actual organization ID from your test setup
@@ -176,7 +243,7 @@ public class OrganizationTests {
     // @Test
     // void testRemoveUserFromOrganization() throws Exception {
     //     String orgId = "organization-id"; // Replace with actual organization ID from your test setup
-    //     String removeUserId = "user-id"; // Replace with actual user ID from your test setup
+    //     String removeWaltId = "user-id"; // Replace with actual user ID from your test setup
     //     mockMvc.perform(MockMvcRequestBuilders.delete("/organization/" + orgId + "/user/" + removeUserId)
     //             .header("Authorization", "Bearer " + token)
     //             .contentType(MediaType.APPLICATION_JSON))
@@ -187,7 +254,11 @@ public class OrganizationTests {
     @AfterAll
     void cleanup() {
         // Clean up the database by deleting the created user
-        User user = userRepository.findByEmail(newUserEmail).orElse(null);
+        User user = userRepository.findByEmail("walter@white.com").orElse(null);
+        if (user != null) {
+            userRepository.delete(user);
+        }
+        user = userRepository.findByEmail("jessie@pinkman.com").orElse(null);
         if (user != null) {
             userRepository.delete(user);
         }
