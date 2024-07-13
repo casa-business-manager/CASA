69245436c03406bebfcff693d288a6429117fd3e
anlhu <anlhu@ucsc.edu>
2024-07-13 05:30:19 +0000

    correct backend iso parsing

--- backend/src/main/java/com/example/casa/Util/DateConverter.java
+++ backend/src/main/java/com/example/casa/Util/DateConverter.java
@@ -1,15 +1,15 @@
 package com.example.casa.Util;
 
-import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateConverter {
 
-	private static final SimpleDateFormat isoDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-
 	public static Date ISO2Date(String isoString) {
 		try {
-			return isoDateFormatter.parse(isoString);
+			ZonedDateTime zonedDateTime = ZonedDateTime.parse(isoString, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+			return Date.from(zonedDateTime.toInstant());
 		} catch(Exception e) {
 			return new Date(0);
 		}
