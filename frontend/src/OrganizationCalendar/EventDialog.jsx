a165c600c3d78ff4bcebee56c1694c8030b70ba4
anlhu <anlhu@ucsc.edu>
2024-07-15 21:14:35 +0000

    location autocomplete

--- frontend/src/OrganizationCalendar/EventDialog.jsx
+++ frontend/src/OrganizationCalendar/EventDialog.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from '@mui/material';
-import Box from '@mui/material/Box';
-import MenuItem from '@mui/material/MenuItem';
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button, Box, MenuItem } from '@mui/material';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Autocomplete } from '@mui/material';
 import dayjs from 'dayjs';
 import ViewHeadlineIcon from '@mui/icons-material/ViewHeadline';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
@@ -16,8 +15,8 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
   const [title, setTitle] = useState(initialTitle);
   const [description, setDescription] = useState(initialDescription);
   const [location, setLocation] = useState(initialLocation);
-  const [startTime, setStartTime] = React.useState(dayjs('2022-04-17T15:30'));
-  const [endTime, setEndTime] = React.useState(dayjs('2022-04-17T15:30'));
+  const [startTime, setStartTime] = useState(dayjs('2022-04-17T15:30'));
+  const [endTime, setEndTime] = useState(dayjs('2022-04-17T15:30'));
 
   useEffect(() => {
     setTitle(initialTitle);
@@ -30,11 +29,8 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
     setLocation('');
   };
 
-  const organizations = [
-    "Personal",
-    "org 1",
-    "Org 2"
-  ];
+  const organizations = ["Personal", "org 1", "Org 2"];
+  const locationSuggestions = ["Location 1", "Location 2", "Location 3"]; // can turn this into a map for "Create Zoom" -> GET new link
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth>
@@ -50,13 +46,13 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
           variant="standard"
           onChange={(e) => setTitle(e.target.value)}
           sx={{ marginBottom: 2 }}
-          InputProps={{ sx: {fontSize: '1.5rem'} }}
-          InputLabelProps={{ sx: {fontSize: '1.5rem'} }}
+          InputProps={{ sx: { fontSize: '1.5rem' } }}
+          InputLabelProps={{ sx: { fontSize: '1.5rem' } }}
         />
 
         {/* Description */}
         <Box sx={{ display: 'flex', alignItems: 'flex-start', marginBottom: 4 }}>
-          <ViewHeadlineIcon sx={{ color: 'action.active', mr: 1.5, my: 2.5 }} />
+          <ViewHeadlineIcon sx={{ color: 'action.active', mr: 1, my: 2.5 }} />
           <TextField
             label="Description"
             multiline
@@ -67,10 +63,10 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
         </Box>
 
         {/* Time Picker */}
-        <Box sx={{ display: 'flex', alignItems: 'flex-start'}}>
-          <AccessTimeFilledIcon sx={{ color: 'action.active', mr: 1.5, my: 2 }} />
+        <Box sx={{ display: 'flex', alignItems: 'flex-start' }}>
+          <AccessTimeFilledIcon sx={{ color: 'action.active', mr: 1, my: 2 }} />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Box sx={{ mr: 2 }}>
+            <Box sx={{ mr: 2 }}>
               <TimePicker
                 label="Start Time"
                 value={startTime}
@@ -89,23 +85,31 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
 
         {/* Location */}
         <Box sx={{ display: 'flex', alignItems: 'flex-start'}}>
-          <LocationOnIcon sx={{ color: 'action.active', mr: 1.5, my: 3.5 }} />
-          <TextField
-            margin="dense"
-            label="Location"
-            type="text"
+          <LocationOnIcon sx={{ color: 'action.active', mr: 1, my: 3.5 }} />
+          <Autocomplete
+            freeSolo
+            options={locationSuggestions}
+            inputValue={location}
             fullWidth
-            multiline
-            maxRows={2}
-            value={location}
-            variant="standard"
-            onChange={(e) => setLocation(e.target.value)}
+            onInputChange={(event, newInputValue) => setLocation(newInputValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                margin="dense"
+                label="Location"
+                type="text"
+                fullWidth
+                multiline
+                maxRows={2}
+                variant="standard"
+              />
+            )}
           />
         </Box>
 
         {/* People */}
-        <Box sx={{ display: 'flex', alignItems: 'flex-start'}}>
-          <PeopleAltIcon sx={{ color: 'action.active', mr: 1.5, my: 3.5 }} />
+        <Box sx={{ display: 'flex', alignItems: 'flex-start' }}>
+          <PeopleAltIcon sx={{ color: 'action.active', mr: 1, my: 3.5 }} />
           <TextField
             margin="dense"
             label="People"
@@ -117,7 +121,7 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
 
         {/* Organization */}
         <Box sx={{ display: 'flex', alignItems: 'flex-start' }}>
-          <ApartmentIcon sx={{ color: 'action.active', mr: 1.5, my: 2.7 }} />
+          <ApartmentIcon sx={{ color: 'action.active', mr: 1, my: 2.7 }} />
           <TextField
             select
             label="Organization"
@@ -136,14 +140,14 @@ const EventDialog = ({ open, onClose, onSave, initialTitle = '', initialDescript
 
       <DialogActions>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-          <Button onClick={onClose} color="error" variant='contained'>
+          <Button onClick={onClose} color="error" variant="contained">
             Delete
           </Button>
           <Box>
             <Button onClick={onClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleSave} color="primary" variant='contained'>
+            <Button onClick={handleSave} color="primary" variant="contained">
               Save
             </Button>
           </Box>
