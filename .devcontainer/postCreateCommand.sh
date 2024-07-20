87c10ff7f4763f9a077eab93fb12e21f8869474a
anlhu <anlhu@ucsc.edu>
2024-07-20 18:44:30 +0000

    remove RUN

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Git settings
-RUN git config --global push.default current 
+git config --global push.default current 
 
 # install frontend dependencies
 (cd frontend && npm install --verbose);
