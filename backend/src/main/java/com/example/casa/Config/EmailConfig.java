6a72a685ee9f2002c62698be9c1ecf7d4ae14d41
Ian <cchong10@ucsc.edu>
2024-07-21 07:06:32 +0000

    fixing build error

--- backend/src/main/java/com/example/casa/Config/EmailConfig.java
+++ backend/src/main/java/com/example/casa/Config/EmailConfig.java
@@ -1,7 +1,11 @@
 package com.example.casa.Config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.example.casa.Payload.EmailDetails;
 
@@ -10,7 +14,25 @@ import com.example.casa.Payload.EmailDetails;
 public class EmailConfig {
     
     @Bean
-    public EmailDetails EmailDetails(){
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); 
+        mailSender.setPort(587); 
+
+        mailSender.setUsername("casa@example.com");
+        mailSender.setPassword("casa"); 
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    @Bean
+    public EmailDetails emailDetails() {
         return new EmailDetails();
     }
 }
