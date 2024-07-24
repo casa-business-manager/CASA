9149381da1118914ed609cafcc0fd619d649abe7
Andrew Hu <anlhu@ucsc.edu>
2024-07-24 02:05:59 +0000

    use postAttach in case things change

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,6 @@
 
 # Git settings
 git config --global push.default current 
-git config --global --add safe.directory ${containerWorkspaceFolder}
 
 # install frontend dependencies
 (cd frontend && npm install --verbose);
