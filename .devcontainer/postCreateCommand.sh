b415785de9edc248e0d56c7e41d5e1083578e1df
Andrew Hu <anlhu@ucsc.edu>
2024-07-24 03:23:50 +0000

    remove old commands

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Git settings
+# Git settings - should be automatically copied from your local system/WSL2
 # git config --global push.default current 
 
 # install frontend dependencies
