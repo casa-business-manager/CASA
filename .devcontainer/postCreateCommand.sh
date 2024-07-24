17805ee8b2a1b0f06b9655d61ba24f868dfd89e6
Andrew Hu <anlhu@ucsc.edu>
2024-07-24 01:56:58 +0000

    copy name and email

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,7 @@
 
 # Git settings
 git config --global push.default current 
+git config --global --add safe.directory ${containerWorkspaceFolder}
 
 # install frontend dependencies
 (cd frontend && npm install --verbose);
