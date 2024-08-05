2362e66566944571d8c07ecd62805e0180d2d81a
Andrew Hu <anlhu@ucsc.edu>
2024-08-05 11:43:48 +0000

    new template selection method

--- frontend/src/EmailEditorPage/EmailEditorPage.jsx
+++ frontend/src/EmailEditorPage/EmailEditorPage.jsx
@@ -1,60 +1,93 @@
-import { Typography, Box, TextField } from "@mui/material";
+import { useParams } from "react-router-dom";
+import {
+	Typography,
+	Box,
+	TextField,
+	List,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+} from "@mui/material";
 import RichTextEditor from "./RichTextEditor";
-import { Grid } from "@mui/material";
-import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
-import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
+import OrganizationTab from "../Settings/SettingTabs/OrganizationTab";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import { useState } from "react";
 
-const TemplateGrid = () => {
-	const templates = Array.from({ length: 12 });
-
+const TemplateTab = ({ name, file, setTemplatePreview }) => {
+	console.log(`name is ${name}`);
 	return (
-		<Box
-			sx={{
-				borderRadius: "8px",
-				border: "1px solid #ccc",
-				boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-				overflowX: "auto",
-				padding: 1,
-				mt: 1,
+		<ListItemButton
+			selected={false}
+			onClick={() => {
+				setTemplatePreview(file);
 			}}
 		>
-			<Grid container spacing={2}>
-				{templates.map((_, index) => (
-					<Grid
-						item
-						xs={12}
-						sm={6}
-						md={Math.floor(templates.length / 3) + 1}
-						lg={3}
-						key={index}
-					>
-						<Box
-							sx={{
-								border: "1px solid #ccc",
-								borderRadius: "8px",
-								padding: "16px",
-								boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-								mt: 1,
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "space-between",
-							}}
-						>
-							<Typography>Template name</Typography>
-							<RadioButtonUncheckedIcon />
-						</Box>
-					</Grid>
-				))}
-			</Grid>
+			<ListItemIcon>
+				<ArticleOutlinedIcon />
+			</ListItemIcon>
+			<ListItemText primary={name} />
+		</ListItemButton>
+	);
+};
+
+const TemplateGrid = ({}) => {
+	const [templates, setTemplates] = useState([
+		{ name: "test", file: "test contents here" },
+		{ name: "test2", file: "test2 contents here" },
+		{ name: "test3", file: "test3 contents here" },
+	]);
+	const [templatePreview, setTemplatePreview] = useState(
+		"Please select a template to preview",
+	);
+
+	console.log(templates);
+
+	return (
+		<Box>
+			<Typography variant="h4">Email Templates</Typography>
+			<Box
+				sx={{
+					borderRadius: "8px",
+					border: "1px solid #ccc",
+					boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+					overflowX: "auto",
+					padding: 1,
+					mt: 1,
+					display: "flex",
+					height: "35vh",
+					gap: 2,
+				}}
+			>
+				<List
+					sx={{
+						width: "35%",
+						overflow: "auto",
+					}}
+					component="nav"
+					aria-labelledby="nested-list-subheader"
+				>
+					{templates.map((template, index) => (
+						<TemplateTab
+							name={template.name}
+							file={template.file}
+							setTemplatePreview={setTemplatePreview}
+							key={index}
+						/>
+					))}
+				</List>
+				<Box>
+					{/* TODO: Read only slate box to display xml or whatever format */}
+					<Typography>Template preview</Typography>
+					<Typography>{templatePreview}</Typography>
+				</Box>
+			</Box>
 		</Box>
 	);
 };
 
-const EmailEditorPage = ({}) => {
+const EmailEditor = ({ sx }) => {
 	return (
-		<>
-			<Typography variant="h4">Email Templates</Typography>
-			<TemplateGrid />
+		<Box sx={sx}>
 			<TextField label="Subject" fullWidth />
 			<TextField
 				label="Recipients"
@@ -74,6 +107,22 @@ const EmailEditorPage = ({}) => {
 			>
 				<RichTextEditor />
 			</Box>
+		</Box>
+	);
+};
+
+const EmailEditorPage = ({}) => {
+	const { orgId } = useParams();
+
+	return (
+		<>
+			<TemplateGrid />
+			<EmailEditor
+				sx={{
+					mt: 1,
+					//  width: "50%"
+				}}
+			/>
 		</>
 	);
 };
