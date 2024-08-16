27636bed36fe5b4a071ff9607f8d9806807d58ca
Andrew Hu <anlhu@ucsc.edu>
2024-08-16 05:08:07 +0000

    fix vertical centering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,32 +14,45 @@ import OrganizationCalendar from "./Calendars/OrganizationCalendar";
 import EmailPage from "./Email/EmailPage";
 import NavBar from "./NavBar/NavBar";
 import OrganizationsContext from "./Contexts/OrganizationsContext";
+import { Box } from "@mui/material";
 
 function App() {
 	const [organizations, setOrganizations] = useState([]);
 
 	return (
-		<Router>
-			<OrganizationsContext.Provider value={[organizations, setOrganizations]}>
-				<NavBar title="Organization Calendar" />
-				<Routes>
-					<Route path="/" element={<Navigate replace to="/login" />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/organization" element={<Organization />} />
-					<Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
-					<Route
-						path="/organization/:orgId"
-						element={<OrganizationLanding />}
-					/>
-					<Route
-						path="/organization/:orgId/calendar"
-						element={<OrganizationCalendar />}
-					/>
-					<Route path="/user/:userId/calendar" element={<UserCalendar />} />
-					<Route path="/organization/:orgId/email" element={<EmailPage />} />
-				</Routes>
-			</OrganizationsContext.Provider>
-		</Router>
+		<Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+			<Router>
+				<OrganizationsContext.Provider
+					value={[organizations, setOrganizations]}
+				>
+					<NavBar title="Organization Calendar" />
+					<Box sx={{ display: "flex", flexGrow: 1, px: 1 }}>
+						<Routes>
+							<Route path="/" element={<Navigate replace to="/login" />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/organization" element={<Organization />} />
+							<Route
+								path="/oauth2/redirect"
+								element={<OAuth2RedirectHandler />}
+							/>
+							<Route
+								path="/organization/:orgId"
+								element={<OrganizationLanding />}
+							/>
+							<Route
+								path="/organization/:orgId/calendar"
+								element={<OrganizationCalendar />}
+							/>
+							<Route path="/user/:userId/calendar" element={<UserCalendar />} />
+							<Route
+								path="/organization/:orgId/email"
+								element={<EmailPage />}
+							/>
+						</Routes>
+					</Box>
+				</OrganizationsContext.Provider>
+			</Router>
+		</Box>
 	);
 }
 
--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -20,7 +20,6 @@ import loginImage from "../Assets/loginScreen.jpg";
 
 const validateEmail = (email) => {
 	// https://mailtrap.io/blog/react-native-email-validation/
-	// eslint-disable-next-line no-control-regex
 	const expression =
 		/(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 	return expression.test(String(email).toLowerCase());
@@ -378,14 +377,24 @@ function Login() {
 	);
 
 	return (
-		<Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-			<Box component="img" src={loginImage} sx={{ width: "75%" }} />
-			<Box sx={{ width: "25%", mx: 4 }}>
-				{!authState
-					? GetStarted
-					: authState === "signIn"
-						? LoginFields
-						: SignUpFields}
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				flexGrow: 1,
+				justifyContent: "center",
+				height: "100%",
+			}}
+		>
+			<Box sx={{ display: "flex", alignItems: "center", gap: 4, px: 2 }}>
+				<Box component="img" src={loginImage} sx={{ width: "70%" }} />
+				<Box sx={{ width: "30%", pr: 6 }}>
+					{!authState
+						? GetStarted
+						: authState === "signIn"
+							? LoginFields
+							: SignUpFields}
+				</Box>
 			</Box>
 		</Box>
 	);
