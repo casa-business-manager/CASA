dbfd036654a939aa769f8e016800ef4e61ee5af7
anlhu <130195350+anlhu@users.noreply.github.com>
2024-08-16 18:57:53 -0700

    Fix login (#65)
    * refactor signup/login, use mui instead of html
    
    * trim css, text field focus bug fix
    
    * move css, rename js to jsx
    
    * crop image, fix link and padding
    
    * checkbox error instead of alert
    
    * error on all textfields, working and refactored
    
    * no user icons on navbar for login page
    
    * fix vertical centering
    
    * fix flex bug, rename App.js to App.jsx
    
    * failed sign in uses MUI alert instead
    
    * sign up need relogin bug finally fixed
    
    * fix bug

---
+++ frontend/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./Login/Login";
+import Organization from "./Organization/Organization";
+import OAuth2RedirectHandler from "./Login/OAuth2RedirectHandler";
+import OrganizationLanding from "./OrganizationLanding/OrganizationLanding";
+import UserCalendar from "./Calendars/UserCalendar";
+import OrganizationCalendar from "./Calendars/OrganizationCalendar";
+import EmailPage from "./Email/EmailPage";
+import NavBar from "./NavBar/NavBar";
+import OrganizationsContext from "./Contexts/OrganizationsContext";
+import { Box } from "@mui/material";
+
+function App() {
+	const [organizations, setOrganizations] = useState([]);
+
+	return (
+		<Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+			<Router>
+				<OrganizationsContext.Provider
+					value={[organizations, setOrganizations]}
+				>
+					<NavBar title="Organization Calendar" />
+					<Box sx={{ flexGrow: 1, px: 1 }}>
+						<Routes>
+							<Route path="/" element={<Navigate replace to="/login" />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/organization" element={<Organization />} />
+							<Route
+								path="/oauth2/redirect"
+								element={<OAuth2RedirectHandler />}
+							/>
+							<Route
+								path="/organization/:orgId"
+								element={<OrganizationLanding />}
+							/>
+							<Route
+								path="/organization/:orgId/calendar"
+								element={<OrganizationCalendar />}
+							/>
+							<Route path="/user/:userId/calendar" element={<UserCalendar />} />
+							<Route
+								path="/organization/:orgId/email"
+								element={<EmailPage />}
+							/>
+						</Routes>
+					</Box>
+				</OrganizationsContext.Provider>
+			</Router>
+		</Box>
+	);
+}
+
+export default App;
