dc89fd1a8c69e47a598646e1872dd4c50b440242
Andrew Hu <anlhu@ucsc.edu>
2024-09-05 06:42:42 +0000

    add debugging echos to entrypoint

--- backend/deployment/entrypoint.sh
+++ backend/deployment/entrypoint.sh
@@ -2,22 +2,29 @@
 
 # start the db
 sudo service mariadb start
+echo "1"
 
 # create a database called 'casa'
 sudo mariadb -e "CREATE DATABASE casa"
+echo "2"
 
 # root user with password 'casa'
 basic_single_escape () {
-    # The quoting on this sed command is a bit complex.  Single-quoted strings
-    # don't allow *any* escape mechanism, so they cannot contain a single
-    # quote.  The string sed gets (as argv[1]) is:  s/\(['\]\)/\\\1/g
-    #
-    # Inside a character class, \ and ' are not special, so the ['\] character
-    # class is balanced and contains two characters.
-    echo "$1" | sed 's/\(['"'"'\]\)/\\\1/g'
+	# The quoting on this sed command is a bit complex.  Single-quoted strings
+	# don't allow *any* escape mechanism, so they cannot contain a single
+	# quote.  The string sed gets (as argv[1]) is:  s/\(['\]\)/\\\1/g
+	#
+	# Inside a character class, \ and ' are not special, so the ['\] character
+	# class is balanced and contains two characters.
+	echo "$1" | sed 's/\(['"'"'\]\)/\\\1/g'
 }
 
 esc_pass=`basic_single_escape "casa"`
 sudo mariadb -e  "UPDATE mysql.global_priv SET priv=json_set(priv, '$.plugin', 'mysql_native_password', '$.authentication_string', PASSWORD('$esc_pass')) WHERE User='root';"
+echo "3"
+
 sudo mariadb -e "FLUSH PRIVILEGES;"
+echo "4"
+
 sudo java -jar /app.jar
+echo "5"
