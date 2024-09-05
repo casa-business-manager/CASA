2700131a0a2ac113d8e501f70700835a39cdfe52
Andrew Hu <anlhu@ucsc.edu>
2024-09-05 20:22:19 +0000

    create org uses org with roles payload

--- backend/src/main/java/com/example/casa/Controller/OrganizationController.java
+++ backend/src/main/java/com/example/casa/Controller/OrganizationController.java
@@ -5,11 +5,8 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -94,7 +91,11 @@ public class OrganizationController {
 		System.out.println("Organization created: " + organization.getOrgName());
 		System.out.println("User organizations: " + user.getOrganizations().size());
 
-		return ResponseEntity.ok(organization);
+		OrganizationWithRoles orgInfo = new OrganizationWithRoles(organization);
+		orgInfo.setUsers(organization.getUsers());
+		orgInfo.setRoles(organization.getRoles());
+
+		return ResponseEntity.ok(orgInfo);
 	}
 
 	@PostMapping("/updateOrganization/organization/{orgId}")
